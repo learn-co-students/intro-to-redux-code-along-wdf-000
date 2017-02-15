88670f4539c77024629a0408bc8885dd329e571f
parm530 <parmsahadeo530@gmail.com>
2017-02-15 16:32:18 -0500

    Done

--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux';
+import { getShoppingListItems } from './actions/shoppingListItemActions';
 import App from './App';
 import './index.css';
 
+import configureStore from './stores/configureStore';
+
+const store = configureStore();
+
+store.dispatch(getShoppingListItems())
+
 ReactDOM.render(
-   <App />,
+   <Provider store={store} >
+   <App />
+   </Provider>,
   document.getElementById('root')
 );
 
