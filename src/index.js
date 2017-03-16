4dfd7117b74cbbfad7c50e233377687d4cd6c446
sami4thewin <samiomarmohammed@gmail.com>
2017-03-16 07:27:50 -0400

    Working on building the code along

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom'
+import configureStore from './stores/configureStore';
 import { Provider } from 'react-redux';
 import App from './App';
 import './index.css';
 
+const store = configureStore();
+
+
 ReactDOM.render(
-   <App />,
+  <Provider store={store}>
+   <App />
+  </Provider>,
   document.getElementById('root')
 );
-
-
